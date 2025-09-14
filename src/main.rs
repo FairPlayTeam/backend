e7bb13cd6be9950fa264c24144d1c071513c29b0
speedy-lex <78314533+speedy-lex@users.noreply.github.com>
2025-09-14 18:31:31 +0200

    basic rest api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::{net::Ipv4Addr, sync::Arc};
+
+use axum::{Json, Router, extract::State, routing::post, serve::Listener};
+use serde::{Deserialize, Serialize};
+use tokio::{net::TcpListener, sync::RwLock};
+
+#[derive(Debug, Default, Clone)]
+struct AppState {
+    value: Arc<RwLock<f64>>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, Clone, Copy)]
+struct Payload {
+    value: f64,
+}
+
+#[tokio::main]
+async fn main() {
+    let value = Arc::new(RwLock::const_new(0.0));
+
+    let app = Router::new()
+        .route("/value", post(put_value).get(get_value))
+        .with_state(AppState {
+            value: value.clone(),
+        });
+
+    axum::serve(
+        TcpListener::bind((Ipv4Addr::new(0, 0, 0, 0), 8080))
+            .await
+            .unwrap(),
+        app,
+    )
+    .await
+    .unwrap()
+}
+
+async fn put_value(
+    State(AppState { value: state }): State<AppState>,
+    Json(payload): Json<Payload>,
+) {
+    *state.write().await = payload.value;
+}
+async fn get_value(State(state): State<AppState>) -> Json<f64> {
+    Json(*state.value.read().await)
 }
